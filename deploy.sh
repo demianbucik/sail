e78abfd630456199e36ed390888c0ab77b16c6e9
demianbucik <demian.bucik@gmail.com>
2021-11-21 01:38:33 +0100

    Use Go 1.16, update deps

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 
 gcloud functions \
     deploy \
-    send-email \
+    staging-send-email \
     --source=. \
     --region=europe-west3 \
     --trigger-http \
     --allow-unauthenticated \
     --entry-point=SendEmailHandler \
-    --memory=256MB \
-    --runtime=go113 \
-    --timeout=10 \
+    --memory=128MB \
+    --runtime=go116 \
+    --timeout=5 \
     --max-instances=2 \
     --env-vars-file=./env.yaml
--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
 module github.com/demianbucik/sail
 
-go 1.13
+go 1.16
 
 require (
+	github.com/apex/log v1.9.0
 	github.com/gorilla/schema v1.2.0
-	github.com/sendgrid/rest v2.6.2+incompatible // indirect
+	github.com/sendgrid/rest v2.6.2+incompatible
 	github.com/sendgrid/sendgrid-go v3.7.2+incompatible
 	github.com/stretchr/testify v1.7.0 // indirect
 	gopkg.in/ezzarghili/recaptcha-go.v4 v4.3.0
